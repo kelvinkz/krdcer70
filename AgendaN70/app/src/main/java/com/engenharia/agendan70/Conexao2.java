b29d8bacdf8d06986c851859f846328b7286b3b5
Rangel Martins Macedo Dias <rangel.dias@bateforte.local>
2016-05-06 14:31:48 -0400

    Inclusao dados conectionstring - sem teste

--- AgendaN70/app/src/main/java/com/engenharia/agendan70/Conexao2.java
+++ AgendaN70/app/src/main/java/com/engenharia/agendan70/Conexao2.java
@@ -13,32 +13,82 @@ public class Conexao2  extends SQLiteOpenHelper{
 
     private static final String TAG = "DBAdapter";
 
-    private static final String NOME_BD = "DBAGENDA";
-    private static final int DATABASE_VERSION = 2;
+    private static String DBPATH = "C:/Transito/Desenvolvimento/Android/krdcer70/";
+    private static final String NOME_BD = "DBAGENDA.sqlite";
+    private static final int DATABASE_VERSION = 1;
 
     Conexao2(Context context)
     {
         super(context, NOME_BD, null, DATABASE_VERSION);
     }
 
+    private void createDataBase(){
+        throws Exception {
+
+            boolean exists = checkDataBase();
+
+            if(!exists) {
+                this.getReadableDatabase();
+
+                try {
+                    copyDatabase();
+                } catch (IOException e) {
+                    throw new Error("Não foi possível copiar o arquivo");
+                }
+            }
+        }
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        /*try {
-            db.execSQL(DATABASE_CREATE);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
+        null;
+    }
+
+    public DatabaseHelper(Context context) {
+	    super(context, NOME_BD, null, 1);
+        this.context = context;
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
-        /*Log.w(TAG, "Atualizando banco de dados da versão " + oldVersion + " para "
-                + newVersion + ". Todos os dados serão destruidos.");
-        db.execSQL("DROP TABLE IF EXISTS pessoas");
-        onCreate(db);*/
+        null;
     }
 
+    private boolean checkDataBase() {
 
+        SQLiteDatabase db = null;
+
+        try {
+            String path = DBPATH + NOME_BD;
+            db = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
+            db.close();
+        } catch (SQLiteException e) {
+            // O banco não existe
+        }
+
+        return db != null;
+    }
+
+    private void copyDatabase(){
+
+        throws IOException {
+
+            String dbPath = DBPATH + NOME_BD;
+            OutputStream dbStream = new FileOutputStream(dbPath);
+            InputStream dbInputStream = context.getAssets().open("DBAGENDA.sqlite");
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while((length = dbInputStream.read(buffer)) &gt; 0) {
+                dbStream.write(buffer, 0, length);
+            }
+
+            dbInputStream.close();
+            dbStream.flush();
+            dbStream.close();
+        }
+    }
 }
