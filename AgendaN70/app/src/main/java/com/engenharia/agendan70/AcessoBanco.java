36dc1f6d9831cf70c75513363fca8441c4185f6b
CarlosDias01 <i.am.the.carlos@gmail.com>
2016-05-17 20:27:52 -0400

    Teste Banco Novo

--- AgendaN70/app/src/main/java/com/engenharia/agendan70/AcessoBanco.java
+++ AgendaN70/app/src/main/java/com/engenharia/agendan70/AcessoBanco.java
@@ -1,74 +1,49 @@
 package com.engenharia.agendan70;
 
-    import android.content.ContentValues;
-    import android.content.Context;
-    import android.database.Cursor;
-    import android.database.SQLException;
-    import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-    public class AcessoBanco {
+import java.util.ArrayList;
+import java.util.List;
 
-        public static final String CODIGO = "codigo";
-        public static final String NOME = "nome";
-        private static final String NOME_BD = "empresa";
-        private static final String NOME_TABELA = "pessoas";
+public class AcessoBanco {
 
-        private final Context context;
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static AcessoBanco instance;
 
-        private Conexao conector;
-        private SQLiteDatabase db;
+    private AcessoBanco(Context context) {
+        this.openHelper = new Conexao(context);
+    }
 
-        public AcessoBanco(Context ctx)
-        {
-            this.context = ctx;
-            conector = new Conexao(context);
+    public static AcessoBanco getInstance(Context context) {
+        if (instance == null) {
+            instance = new AcessoBanco(context);
         }
+        return instance;
+    }
 
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
 
-        public AcessoBanco open() throws SQLException
-        {
-            db = conector.getWritableDatabase();
-            return this;
-        }
-
-        public void close()
-        {
-            conector.close();
-        }
-
-        public long inserePessoa(String name, int codigo)
-        {
-            ContentValues initialValues = new ContentValues();
-            initialValues.put(NOME, name);
-            initialValues.put(CODIGO, codigo);
-            return db.insert(NOME_TABELA, null, initialValues);
-        }
-
-        public boolean removePessoa(long cod)
-        {
-            return db.delete(NOME_TABELA, CODIGO + "=" + cod, null) > 0;
-        }
-
-        public Cursor getCadastros()
-        {
-            return db.query(NOME_TABELA, new String[] {CODIGO, NOME}, null, null, null, null, null);
-        }
-
-        public Cursor getPessoa(long cod) throws SQLException
-        {
-            Cursor mCursor =
-                    db.query(true, NOME_TABELA, new String[] {CODIGO, NOME}, CODIGO + "=" + cod, null,
-                            null, null, null, null);
-            if (mCursor != null) {
-                mCursor.moveToFirst();
-            }
-            return mCursor;
-        }
-
-        public boolean updatePessoa(long cod, String name, String email)
-        {
-            ContentValues args = new ContentValues();
-            args.put(NOME, name);
-            return db.update(NOME_BD, args, CODIGO + "=" + cod, null) > 0;
+    public void close() {
+        if (database != null) {
+            this.database.close();
         }
     }
+
+    public List<String> getQuotes() {
+        List<String> list = new ArrayList<>();
+        Cursor cursor = database.rawQuery("SELECT * FROM quotes", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+}
