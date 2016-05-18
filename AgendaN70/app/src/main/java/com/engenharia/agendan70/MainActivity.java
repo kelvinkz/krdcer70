2d53f0917845ab5e747addeca00f7978cc2fcaf5
Alien <Alien>
2016-05-18 00:09:40 -0400

    Mudando Main activity para o Calendar e criando classe de permissoes

--- AgendaN70/app/src/main/java/com/engenharia/agendan70/MainActivity.java
+++ AgendaN70/app/src/main/java/com/engenharia/agendan70/MainActivity.java
@@ -1,21 +1,44 @@
 package com.engenharia.agendan70;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final AcessoBanco db = AcessoBanco.getInstance(this);
+        initializeCalendar();
     }
 
-    public void gotoToCalendario() {
-        Intent intent = new Intent(this, CalendarActivity.class);
-        startActivity(intent);
+    public void initializeCalendar() {
+        calendar = (CalendarView) findViewById(R.id.calendar);
+
+        calendar.setShowWeekNumber(false);
+
+        calendar.setFirstDayOfWeek(2);
+
+        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
+
+        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.blue));
+
+        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.blue));
+
+        calendar.setSelectedDateVerticalBar(R.color.darkgreen);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            //show the selected date as a toast
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
