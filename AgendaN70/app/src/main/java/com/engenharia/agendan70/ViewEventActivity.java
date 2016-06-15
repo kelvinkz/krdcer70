5dd9cb5708110a7953d9d50b86e1e23ae2ff2436
Kelvin Zarur <kelvinzarur@hotmail.com>
2016-06-15 11:39:31 -0400

    ultimas alteracoes

--- AgendaN70/app/src/main/java/com/engenharia/agendan70/ViewEventActivity.java
+++ AgendaN70/app/src/main/java/com/engenharia/agendan70/ViewEventActivity.java
@@ -5,17 +5,15 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ViewEventActivity extends AppCompatActivity {
 
     private EditText campoTitulo;
     private EditText campoLocal;
-    private Spinner campoTipoEvento;
-    private Spinner campoRepeticao;
+    private EditText campoTipoEvento;
+    private EditText campoRepeticao;
     private EditText campoDataInicio;
     private EditText campoParticipante;
     private int id;
@@ -27,16 +25,10 @@ public class ViewEventActivity extends AppCompatActivity {
 
         campoTitulo = (EditText) findViewById(R.id.campoTitulo);
         campoLocal = (EditText) findViewById(R.id.campoLocal);
-        campoTipoEvento = (Spinner) findViewById(R.id.campoTipoEvento);
-        campoRepeticao = (Spinner) findViewById(R.id.campoRepeticao);
+        campoTipoEvento = (EditText) findViewById(R.id.campoTipoEvento);
+        campoRepeticao = (EditText) findViewById(R.id.campoRepeticao);
         campoDataInicio = (EditText) findViewById(R.id.campoData);
         campoParticipante = (EditText) findViewById(R.id.campoParticipante);
-
-        ArrayAdapter<String> adapterTipoEvento = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{"Festa", "Casamento", "Reuniao", "Outro"});
-        campoTipoEvento.setAdapter(adapterTipoEvento);
-
-        ArrayAdapter<String> adapterRepeticao = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{"Sem repetição", "Diariamente", "Semanalmente", "Mensalmente", "Anualmente"});
-        campoRepeticao.setAdapter(adapterRepeticao);
 
         Bundle bundle = getIntent().getExtras();
         id = bundle.getInt("ID");
@@ -52,6 +44,8 @@ public class ViewEventActivity extends AppCompatActivity {
             campoDataInicio.setText(cursor.getString(cursor.getColumnIndex("DATA")));
             campoTitulo.setText(cursor.getString(cursor.getColumnIndex("DESCRICAO")));
             campoLocal.setText(cursor.getString(cursor.getColumnIndex("LOCAL")));
+            campoTipoEvento.setText(cursor.getString(cursor.getColumnIndex("TIPO_EVENTO")));
+            campoRepeticao.setText(cursor.getString(cursor.getColumnIndex("REPETICAO")));
             campoParticipante.setText(cursor.getString(cursor.getColumnIndex("PARTICIPANTES")));
         } else {
             Toast.makeText(getApplicationContext(), "SELECT VAZIO", Toast.LENGTH_LONG).show();
